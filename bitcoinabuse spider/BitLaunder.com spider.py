74f52d687ad13d944f15cf5d6dfeb3a889c67f06
Ammoniya <ravindushantha1995@gmail.com>
2020-06-12 20:47:33 +0530

    BitLaunder.com spider updated

--- bitcoinabuse spider/BitLaunder.com spider.py
+++ bitcoinabuse spider/BitLaunder.com spider.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 
 import json
 import pandas as pd
@@ -14,26 +12,16 @@ from datetime import datetime
 from time import sleep
 
 
-# In[ ]:
-
 
 URL_home = 'http://www.walletexplorer.com/api/1/wallet-addresses?wallet=BitLaunder.com&from=0&count=100&caller=###'
 URL_tx = 'http://www.walletexplorer.com/api/1/address?address={}&from=0&count=100&caller=####'
 
 
-# In[ ]:
-
-
 response = get(URL_home)
-
-
-# In[ ]:
 
 
 data = json.loads(response.text)
 
-
-# In[ ]:
 
 
 for i in tqdm(data.get('addresses')):
@@ -42,21 +30,8 @@ for i in tqdm(data.get('addresses')):
     sleep(2)
 
 
-# In[ ]:
-
-
 FILENAME = datetime.now().strftime("%y-%m-%d")+' BitLaunder.com.json'
-
-
-# In[ ]:
 
 
 with open(FILENAME, 'w') as outfile:
     json.dump(data, outfile)
-
-
-# In[ ]:
-
-
-
-
