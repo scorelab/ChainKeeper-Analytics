e01d5d82fe49ca2860a43d2eb7c5144f05346636
Ammoniya <ravindushantha1995@gmail.com>
2020-07-23 22:23:46 +0530

    simple data insert script #1 - mongodb

---
+++ simple data insert scripts - mongodb/walletexplorer.py
@@ -0,0 +1,23 @@
+import pymongo
+from requests import get
+import time
+import pandas as pd
+import json
+
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["SCAN"]
+col = db["walletexplorer"]
+
+l = [i.split()[0] for i in open('baddr','r').readlines()]
+TOKEN = 'ravindud@scorelab.org'
+
+
+for i in set(l):
+    QUERY = "http://www.walletexplorer.com/api/1/address-lookup?address={}&caller={}"
+    data = get(QUERY.format(i,TOKEN)).text
+    data = json.loads(data)
+    data['adddress']=i
+    col.insert_one(data)
+    print(data)
+    time.sleep(6.0)
